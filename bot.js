3c5a2dfbbce16985e5ffb2b8487f903068818086
10Unknownboy <97281187+10Unknownboy@users.noreply.github.com>
2026-03-13 12:22:18 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,40 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
-  host: 'goondust.play.hosting',
-  username: 'Epstein',
-  version: '1.21.11'
-})
+function createBot() {
 
-bot.on('spawn', () => {
-  console.log('Epstein Entered The Island <3')
-})
+  const bot = mineflayer.createBot({
+    host: 'goondust.play.hosting',
+    username: 'Epstein',
+    version: '1.21.11' // use the exact server version
+  })
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
+  bot.on('spawn', () => {
+    console.log('Epstein Entered The Island <3')
 
-  if (message === 'Goon') {
-    bot.chat('AHHHHHHHHHHHHHHHHHHHHHHHH')
-  }
-})
+    bot.chat('Hello Kids, missed me? <3')
 
-bot.once('spawn', () => {
-  setInterval(() => {
-    bot.chat('/tp Epstein @r')
-  }, 600000) // every 60 seconds
-})
-bot.on('error', console.log)
-bot.on('kicked', console.log)
+    // run command every 10 minutes
+    setInterval(() => {
+      bot.chat('/tp Epstein @r')
+    }, 600000)
+  })
+
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+
+    if (message === 'Goon') {
+      bot.chat('AHHHHHHHHHHHHHHHHHHHHHHHH')
+    }
+  })
+
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
+
+  // auto reconnect
+  bot.on('end', () => {
+    console.log('Bot disconnected... reconnecting in 5 seconds')
+    setTimeout(createBot, 5000)
+  })
+}
+
+createBot()
